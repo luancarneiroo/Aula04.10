4375396b4cd70cebdbfde89a77f89e9055b57e35
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-10 10:59:49 -0300

    fiz umas contas

--- decisao.c
+++ decisao.c
@@ -6,6 +6,17 @@ int main(){
     // if (idade >= 18) {
     //    printf("Maior de idade\n");
     // } 
+
+    float nota_1;
+    float nota_2;
+    printf("CALCULADORA DE MEDIA\n");
+    printf("Digite a primeira nota do aluno:\n");
+    scanf("%f",%nota_1);
+    printf("Digite a segunda nota do aluno:"\n);
+    scanf("%f",%nota_2);
+
+    float media = (nota_1 + nota_2)/2;
+    
     float media = 8;
     if (media >= 6){
         printf("APROVADO");
