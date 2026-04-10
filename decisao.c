934a4b5cb273b34da2c0af8ef595bc2842c03441
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-10 11:48:30 -0300

    fiz tudo repeticao

--- decisao.c
+++ decisao.c
@@ -1,29 +1,52 @@
 #include<stdio.h>
  
 int main(){
-        // AULA DIA 10/04: Tomada de Decisão
-    // int idade = 18;
-    // if (idade >= 18) {
-    //    printf("Maior de idade\n");
-    // } 
+    /* AULA DIA 10/04: Tomada de Decisão
+    int idade = 18;
+    if (idade >= 18) {
+    printf("Maior de idade\n"); 
+     
 
-    float nota_1;
-    float nota_2;
-    printf("CALCULADORA DE MEDIA\n");
-    printf("Digite a primeira nota do aluno:\n");
-    scanf("%f",&nota_1);
-    printf("Digite a segunda nota do aluno:\n");
-    scanf("%f",&nota_2);
-
-    float media = (nota_1 + nota_2)/2;
+    CONDIÇÃO DE PRESENÇA
+    APROVADO --> MAIOR ; IGUAL 75%
+    REPROVADO -> MENOR QUE 75
     
-    if (media >= 6){
+    MÉDIA + PRESENÇA
+    APROVADO -> MEDIA MAIOR OU IGUAL A 6 PRESENCA MAIOR IGUAL 75
+    REPOVADO POR FALTA -----> PRESENÇA INSUFICIENTE
+    REPROVADO POR NOTA -----> MÉDIA INSUFICIENTE
+    REPROVADO TOTAL --------> PRESENÇA E NOTA INSUFICIENTE
+    */
+
+
+   float nota_1;
+   float nota_2;
+   float presenca;
+
+   printf("CALCULADORA DE MEDIA\n");
+   printf("Digite a primeira nota do aluno:\n");
+   scanf("%f",&nota_1);
+   printf("Digite a segunda nota do aluno:\n");
+   scanf("%f",&nota_2);
+   printf("Digite a presença do aluno:\n");
+   scanf("%f",&presenca);
+
+
+   float media = (nota_1 + nota_2)/2;
+
+      if (media >= 6 && presenca >= 75){
         printf("APROVADO");
     } else if (media >= 4 && media > 6) {
         printf("RECUPERAÇÃO");
-    } else{
-        printf("REPROVADO");
-    }
+    } else if(presenca <= 75 && media <= 4) {
+        printf("REPROVADO TOTAL");
+    } else if(presenca <= 75) {
+        printf("REPROVADO POR FALTA");
+    } else if(media <= 4){
+        printf("REPROVADO POR NOTA");
+    } 
+    
+
 
 
     return 0;
--- repeticao.c
+++ repeticao.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
  
 int main(){
+    /*
+    Fazer a tabuada de qualquer número solicitado pelo usuário
+    Usuário entrou com o valor 2:
+    2 x 0 = 0
+    2 x 1 = 2
+    2 x 2 = 4
+    .
+    .
+    .
+    2 x 10 = 20 
+    */
     
-
-
+    for(int i = 0; i < 11; i++){
+        printf("%d\n", i);
+    }
+    // printf("\n%d\n", i);
     return 0;
 }
