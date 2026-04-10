f2ae8daad0a21b7fb5dc77de480536d0adb1bef7
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-10 11:05:20 -0300

    fiz a calculadora de media e deu tudo certo amem :)

--- decisao.c
+++ decisao.c
@@ -11,13 +11,12 @@ int main(){
     float nota_2;
     printf("CALCULADORA DE MEDIA\n");
     printf("Digite a primeira nota do aluno:\n");
-    scanf("%f",%nota_1);
-    printf("Digite a segunda nota do aluno:"\n);
-    scanf("%f",%nota_2);
+    scanf("%f",&nota_1);
+    printf("Digite a segunda nota do aluno:\n");
+    scanf("%f",&nota_2);
 
     float media = (nota_1 + nota_2)/2;
     
-    float media = 8;
     if (media >= 6){
         printf("APROVADO");
     } else if (media >= 4 && media > 6) {
